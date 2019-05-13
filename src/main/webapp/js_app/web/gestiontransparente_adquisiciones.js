bff0bf0ddbd583eff02410c6778e82a5318de28d
JhanxD <JhanxD@192.168.1.61>
2019-05-13 11:51:51 -0500

    update

--- src/main/webapp/js_app/web/gestiontransparente_adquisiciones.js
+++ src/main/webapp/js_app/web/gestiontransparente_adquisiciones.js
@@ -535,69 +535,44 @@ function listarMesOrdenesCompra(BEAN_PAGINATION) {
 }
 
 function traerNombreMes(orden) {
+    var numeromes=orden.trim();
+    var nuemero=parseInt(numeromes);
     var mes;
-    switch (orden) {
-        case "01":
+    switch (nuemero) {
+        case 1:
             mes = "Enero";
             break;
-        case "1 ":
-            mes = "Enero";
-            break;
-        case "2 ":
+        case 2:
             mes = "Febrero";
             break;
-        case "3 ":
+        case 3:
             mes = "Marzo";
             break;
-        case "4 ":
+        case 4:
             mes = "Abril";
             break;
-        case "5 ":
+        case 5:
             mes = "Mayo";
             break;
-        case "6 ":
+        case 6:
             mes = "Junio";
             break;
-        case "7 ":
+        case 7:
             mes = "Julio";
             break;
-        case "8 ":
+        case 8:
             mes = "Agosto";
             break;
-        case "9 ":
+        case 9:
             mes = "Setiembre";
             break;
-        case "02":
-            mes = "Febrero";
-            break;
-        case "03":
-            mes = "Marzo";
-            break;
-        case "04":
-            mes = "Abril";
-            break;
-        case "05":
-            mes = "Mayo";
-            break;
-        case "06":
-            mes = "Junio";
-            break;
-        case "07":
-            mes = "Julio";
-            break;
-        case "08":
-            mes = "Agosto";
-            break;
-        case "09":
-            mes = "Setiembre";
-            break;
-        case "10":
+        case 10:
             mes = "Octubre";
             break;
-        case "11":
+        case 11:
             mes = "Noviembre";
             break;
-        case "12":
+        case 12:
             mes = "Diciembre";
             break;
     }
